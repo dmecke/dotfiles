42263dc6a7ab66573e02a9d7ce68b872ada448c7
Daniel Mecke <mecke@kigaroo.de>
2024-07-08 14:28:54 +0200

    added bat

---
+++ bat/.oh-my-zsh/custom/bat.zsh
@@ -0,0 +1,6 @@
+# ubuntu install the binary as "batcat", so alias it to "bat"
+alias bat="batcat"
+
+# colored man pages
+export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+
---
+++ bat/README.md
@@ -0,0 +1,8 @@
+```
+# https://github.com/sharkdp/bat
+sudo apt install bat
+
+# create symlinks
+cd ~ && stow bat
+```
+

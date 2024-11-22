91ac010fc28b50c6e4494e8ce66badd70d632f5c
Daniel Mecke <daniel.mecke@cunningsoft.de>
2024-11-22 18:39:49 +0100

    added onlinetennis

---
+++ onlinetennis/.oh-my-zsh/custom/onlinetennis.zsh
@@ -0,0 +1,5 @@
+alias onlinetennis_cd="cd /home/dmecke/projects/Onlinetennis"
+alias onlinetennis_start_webpack="onlinetennis_cd && yarn run dev"
+alias onlinetennis_start_mercure="onlinetennis_cd && bin/mercure --jwt-key='vAo9ZRqtKfg4ejIG2g8RQNLt6PoMHFwM' --addr=':3000' --allow-anonymous --cors-allowed-origins='http://www.onlinetennis.localhost'"
+alias onlinetennis_setup="use_php74 && use_node14 && restart_apache && restart_mysql"
+

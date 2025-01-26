692443d6fdb00541dcf2abef9ac95a77348b9763
Daniel Mecke <daniel.mecke@cunningsoft.de>
2025-01-26 21:13:50 +0100

    added php8.4

--- php/.oh-my-zsh/custom/php.zsh
+++ php/.oh-my-zsh/custom/php.zsh
@@ -1,5 +1,5 @@
 # helper
-alias disable_a2mods="sudo a2dismod php7.1 php7.2 php7.3 php7.4 php8.0 php8.1 php8.2 php8.3"
+alias disable_a2mods="sudo a2dismod php7.1 php7.2 php7.3 php7.4 php8.0 php8.1 php8.2 php8.3 php8.4"
 
 # php
 alias use_php71="disable_a2mods && sudo a2enmod php7.1 && restart_apache && sudo update-alternatives --set php /usr/bin/php7.1"
@@ -10,4 +10,5 @@ alias use_php80="disable_a2mods && sudo a2enmod php8.0 && restart_apache && sudo
 alias use_php81="disable_a2mods && sudo a2enmod php8.1 && restart_apache && sudo update-alternatives --set php /usr/bin/php8.1"
 alias use_php82="disable_a2mods && sudo a2enmod php8.2 && restart_apache && sudo update-alternatives --set php /usr/bin/php8.2"
 alias use_php83="disable_a2mods && sudo a2enmod php8.3 && restart_apache && sudo update-alternatives --set php /usr/bin/php8.3"
+alias use_php84="disable_a2mods && sudo a2enmod php8.4 && restart_apache && sudo update-alternatives --set php /usr/bin/php8.4"
 

623867bad03000de5f643b9fb759afd923f0af83
Daniel Mecke <mecke@kigaroo.de>
2025-08-12 08:24:08 +0200

    added new node versions

--- node/.oh-my-zsh/custom/node.zsh
+++ node/.oh-my-zsh/custom/node.zsh
@@ -2,4 +2,6 @@ alias use_node14="nvm use 14"
 alias use_node16="nvm use 16"
 alias use_node18="nvm use 18"
 alias use_node20="nvm use 20"
+alias use_node22="nvm use 22"
+alias use_node24="nvm use 24"
 
